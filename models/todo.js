47292027c47857a7d2387bceeca306d897543349
sindhugouragari <113445889+nivasbanoth@users.noreply.github.com>
2023-01-07 15:23:57 +0530

    kk

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,8 @@
 "use strict";
 const { Model } = require("sequelize");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -15,8 +18,47 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
+    static async remove(id) {
+      return this.destroy({ where: { id } });
+    }
+
+    static async getDueToday() {
+      const d = new Date().toLocaleDateString("en-CA");
+      const today = await this.findAll({
+        where: { dueDate: { [Op.eq]: d }, completed: false },
+      });
+      return today;
+    }
+
+    static async getDueLater() {
+      const d = new Date().toLocaleDateString("en-CA");
+      const later = await this.findAll({
+        where: { dueDate: { [Op.gt]: d }, completed: false },
+      });
+      return later;
+    }
+
+    static async getOverDue() {
+      const d = new Date().toLocaleDateString("en-CA");
+      const overdue = await this.findAll({
+        where: { dueDate: { [Op.lt]: d }, completed: false },
+      });
+      return overdue;
+    }
+
+    static async getCompleted() {
+      const complete = await this.findAll({
+        where: { completed: true },
+      });
+      return complete;
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
+    }
+
+    setCompletionStatus(status) {
+      return this.update({ completed: status });
     }
   }
   Todo.init(
